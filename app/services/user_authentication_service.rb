715c4ce1eefaa3422a16943b58ca1a7c8d4901a0
James Harton <jamesotron@gmail.com>
2013-08-03 12:21:42 +1200

    Document my reservations about the api_token technique in use.

--- app/services/user_authentication_service.rb
+++ app/services/user_authentication_service.rb
@@ -11,6 +11,11 @@ module UserAuthenticationService
     authenticate_with_password(user,password) or raise NotAuthorized
   end
 
+  # FIXME
+  # I'm not entirely happy with this solution, as this means that the api key
+  # will always be exactly the same, making it super simple to MITM.
+  # Suggestions on how to improve it?  Perhaps include a timestamp or something
+  # else that the API client and the server know, but not a third party?
   def authenticate_with_api_key(user, key)
     user && OpenSSL::Digest::SHA256.new("#{user.username}:#{user.api_secret}") == key
   end
