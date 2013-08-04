1b8a5b53584fe0d2e47dbc8cc5812e015cf298d2
James Harton <jamesotron@gmail.com>
2013-08-04 19:33:45 +1200

    Remove the baseline homepage.

--- features/baseline.feature
+++
@@ -1,5 +0,0 @@
-@javascript
-Feature: baseline
-  Scenario: I see the empty home page
-    When I visit the homepage
-    Then I should see the baseline page
--- features/step_definitions/baseline_steps.rb
+++
@@ -1,7 +0,0 @@
-When(/^I visit the homepage$/) do
-  visit '/'
-end
-
-Then(/^I should see the baseline page$/) do
-  expect(page.text).to include('Baseline')
-end
